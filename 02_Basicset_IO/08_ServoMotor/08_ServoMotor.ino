bceda67dcbef3418dc6d526929cd371fbcd6bbad
naito-a <nazusa63@gmail.com>
2025-05-22 15:45:34 +0900

    Chenge speed

--- 02_Basicset_IO/08_ServoMotor/08_ServoMotor.ino
+++ 02_Basicset_IO/08_ServoMotor/08_ServoMotor.ino
@@ -9,14 +9,69 @@ void setup() {
 }
 
 void loop() {
-  // 0-180 right
-  for(int angle = 0; angle <= 180; angle++) {                        
+  // 0-30 right
+  for(int angle = 0; angle <= 30; angle++) {                        
     myservo.write(angle);
-    delay(15);
+    delay(500);
+  }
+  // 30-60 right
+  for(int angle = 30; angle <= 60; angle++) {                        
+    myservo.write(angle);
+    delay(500);
+  }
+  // 60-90 right
+  for(int angle = 60; angle <= 90; angle++) {                        
+    myservo.write(angle);
+    delay(500);
+  }
+  // 90-120 right
+  for(int angle = 90; angle <= 120; angle++) {                        
+    myservo.write(angle);
+    delay(500);
+  }
+  // 120-150 right
+  for(int angle = 120; angle <= 150; angle++) {                        
+    myservo.write(angle);
+    delay(500);
+  }
+  // 150-180 right
+  for(int angle = 150; angle <= 180; angle++) {                        
+    myservo.write(angle);
+    delay(500);
   }
   // 180-0  left 
   for(int angle = 180; angle >= 0; angle--) {                 
     myservo.write(angle);  
-    delay(15);      
+    delay(500);      
   }
+  // 180-150  left 
+  for(int angle = 180; angle >= 150; angle--) {                 
+    myservo.write(angle);  
+    delay(500);      
+  }
+  // 150-120  left 
+  for(int angle = 150; angle >= 120; angle--) {                 
+    myservo.write(angle);  
+    delay(500);      
+  } 
+  // 120-90  left 
+  for(int angle = 120; angle >= 90; angle--) {                 
+    myservo.write(angle);  
+    delay(500);      
+  }
+  // 90-60  left 
+  for(int angle = 90; angle >= 60; angle--) {                 
+    myservo.write(angle);  
+    delay(500); 
+  }
+  // 60-30  left 
+  for(int angle = 60; angle >= 30; angle--) {                 
+    myservo.write(angle);  
+    delay(500);  
+  }
+  // 30-0  left 
+  for(int angle = 30; angle >= 0; angle--) {                 
+    myservo.write(angle);  
+    delay(500);  
+  }    
 }
