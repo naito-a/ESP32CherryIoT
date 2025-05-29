537bb7e2662f5e2a3ce792475523091f85ea2555
naito-a <nazusa63@gmail.com>
2025-05-29 15:59:53 +0900

    try 01

--- 01_Basicset_SW-LED/01_LED-Blink/01_LED-Blink.ino
+++ 01_Basicset_SW-LED/01_LED-Blink/01_LED-Blink.ino
@@ -1,12 +1,44 @@
-const int ledPin = 3; //3:ConnectorA 4:ConnectorB 10:Builtin
-
 void setup() {
-  pinMode(ledPin, OUTPUT);
+    pinMode(13,OUTPUT);
+    pinMode(11,OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(8,OUTPUT);
+    pinMode(7,OUTPUT);
+
+    pinMode(13,OUTPUT);
+    pinMode(11,OUTPUT);
+    pinMode(10,OUTPUT);
+    pinMode(8,OUTPUT);
+    pinMode(7,OUTPUT); 
 }
 
 void loop() {
-  digitalWrite(ledPin, HIGH); //ON
-  delay(100); //0.5sec
-  digitalWrite(ledPin, LOW); //OFF
-  delay(100); //0.5sec
+  digitalWrite(12, HIGH); //ON 青
+  digitalWrite(2, HIGH); //ON 歩行者青
+  delay(2000); //2秒
+  digitalWrite(2, LOW); //OFF 歩行者点滅
+  delay(500); //2秒
+  digitalWrite(2, HIGH); //ON
+  delay(500); //2秒
+  digitalWrite(2, LOW); //OFF
+  delay(500); //2秒
+  digitalWrite(2, HIGH); //ON
+  delay(500); //2秒
+  digitalWrite(2, LOW); //OFF
+  digitalWrite(3, HIGH); //ON　歩行者赤
+  delay(700); //
+  digitalWrite(12, LOW); //OFF
+  digitalWrite(10, HIGH); //ON　黄
+  delay(2000); //2秒
+  digitalWrite(10, LOW); //OFF
+  digitalWrite(8, HIGH); //ON　赤
+  delay(5000); //５秒
+  digitalWrite(8, LOW); //OFF
+  digitalWrite(3, LOW); //OFF
+
+
+
+
+ 
 }
+
--- 01_Basicset_SW-LED/07_SW-LED/07_SW-LED.ino
+++ 01_Basicset_SW-LED/07_SW-LED/07_SW-LED.ino
@@ -1,20 +1,22 @@
-const int swPin = 3; //3:ConnectorA 4:ConnectorB
+const int litsnsrPin = 3; //3:ConnectorA 4:ConnectorB
 const int ledPin = 4; //3:ConnectorA 4:ConnectorB 10:Builtin
 
 void setup() {
   Serial.begin(115200);
-  pinMode(swPin, INPUT);
+  pinMode(litsnsrPin, INPUT);
   pinMode(ledPin, OUTPUT);
 }
 
 void loop() { 
-  if (digitalRead(swPin) == HIGH) {
-    digitalWrite(ledPin, HIGH); //ON
-    Serial.println("Pushed");
+  float litsnsr_ad = analogRead(litsnsrPin); // Read analog data
+  float litsnsr_v = litsnsr_ad * 3.3 / 4096; // Calculation of voltage value
+  float lux = 10000 * litsnsr_v / (3.3 - litsnsr_v) / 1000; // Calculation of lux value
+
+  if (lux < 700 == LOW) {
+    digitalWrite(ledPin, LOW); //ON
     delay(300);
   } else {
-    digitalWrite(ledPin, LOW); //OFF
-    Serial.println("Not Pushed");
+    digitalWrite(ledPin,HIGH); //OFF
     delay(300);
   }
 }
