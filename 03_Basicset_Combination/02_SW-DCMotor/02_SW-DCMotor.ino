e1a3e0108275e6def9e936d1ec6e59baba314bda
naito-a <nazusa63@gmail.com>
2025-06-05 11:54:16 +0900

    Try 02

--- 03_Basicset_Combination/02_SW-DCMotor/02_SW-DCMotor.ino
+++ 03_Basicset_Combination/02_SW-DCMotor/02_SW-DCMotor.ino
@@ -1,28 +1,20 @@
-const int swPin = 3; //3:ConnectorA 4:ConnectorB
-const int motorPin = 4; //3:ConnectorA 4:ConnectorB
-
-bool swState;
+const int motorPin = 3; //3:ConnectorA 4:ConnectorB
+const int swPin = 4; //3:ConnectorA 4:ConnectorB 10:Builtin
 
 void setup() {
   Serial.begin(115200);
-  pinMode(swPin, INPUT);
+  pinMode(swPin, INPUT);;
   pinMode(motorPin, OUTPUT);
 }
 
 void loop() {
-
   if (digitalRead(swPin) == HIGH) {
-    swState = !swState;
-  }
-
-  if (swState == 1) { 
-    digitalWrite(motorPin, HIGH); //ON
-    Serial.println("ON");
-    delay(300);
+    Serial.println("Pushed"); 
+    digitalWrite(motorPin, HIGH);
+    delay(200);
   } else {
-    digitalWrite(motorPin, LOW); //OFF
-    Serial.println("OFF");
-    delay(300);
+    Serial.println("Not Pushed");
+    digitalWrite(motorPin, LOW);
+    delay(200);
   }
-  
 }
